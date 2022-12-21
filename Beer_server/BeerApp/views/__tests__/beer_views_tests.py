c7fc296ff40233592d4dc7b93db2ddb922664162
Micka <lucasmichaeli95@gmail.com>
2022-12-21 12:11:14 +0100

    30 - change place api beer tests

--- Beer_server/BeerApp/views/__tests__/beer_views_tests.py
+++ Beer_server/BeerApp/views/__tests__/beer_views_tests.py
@@ -1,7 +1,5 @@
-from django.test import TestCase, RequestFactory
-from django.urls import reverse
+from django.test import TestCase
 from rest_framework.test import APIRequestFactory
-import json
 
 from BeerApp.utils.factory.beer_factory import BeerFactory
 from BeerApp.models.beer_model import Beer
@@ -12,14 +10,6 @@ class TestBeerViewSet(TestCase):
     @classmethod
     def setUpTestData(cls) -> None:
         BeerFactory()
-
-    def test_get_method_beer_api_url(self):
-        factory_request = APIRequestFactory()
-        view = BeerViewSet.as_view({'get': 'list'})
-
-        request = factory_request.get("/data/get_all_beers/")
-        response = view(request)
-        self.assertEqual(response.status_code, 200)
 
     def test_get_method_beer_api_response_data(self):
         factory_request = APIRequestFactory()
